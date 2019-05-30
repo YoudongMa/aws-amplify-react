b952fb23c2cba8624b0641c88b0a3687d2f105be
youdong.ma <Youdong.Ma@ama.ab.ca>
2019-05-30 13:29:02 -0600

    remove auth

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 import Amplify from 'aws-amplify';
 import awsmobile from './aws-exports';
-import { withAuthenticator } from 'aws-amplify-react'; // or 'aws-amplify-react-native';
 
 Amplify.configure(awsmobile);
 
@@ -28,4 +27,4 @@ function App() {
   );
 }
 
-export default withAuthenticator(App, true);
+export default App;
